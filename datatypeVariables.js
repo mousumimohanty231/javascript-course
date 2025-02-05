0f52aed78498fb8130d68efc215aba6974209226
Mousumi Mohanty <mousumimohanty231@gmail.com>
2025-02-05 19:52:21 +0530

    Add files via upload

---
+++ datatypeVariables.js
@@ -0,0 +1,26 @@
+console.log("Hello world")
+//inline comment
+/*
+this is
+multiline comment */
+
+
+//variable declaration
+var myName = "Mama"
+console.log(myName)
+myName = 8
+let youName = "Rima"
+const pi = 3.14
+console.log(myName)
+
+
+//declaration
+var a;
+
+//assigenment
+a = 9;
+//decrement a--
+//increment
+a++
+console.log(a)
+
